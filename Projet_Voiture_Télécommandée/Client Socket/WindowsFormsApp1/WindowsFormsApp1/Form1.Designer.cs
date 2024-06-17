ffb50a424e5d1d1a55413f11370cc9d25bb5af89
svmi <sami.blh.212@gmail.com>
2024-06-17 15:36:37 +0200

    Add files via upload

---
+++ Projet_Voiture_Télécommandée/Client Socket/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(211, 14);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 18;
+            this.txtPort.Text = "212";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Port :";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(55, 14);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 20);
+            this.txtHost.TabIndex = 16;
+            this.txtHost.Text = "10.194.196.56";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Host :";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(418, 12);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 20;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(55, 79);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(347, 118);
+            this.txtData.TabIndex = 21;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(438, 122);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 22;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Controller Connection :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(241, 203);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 24;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(438, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 325);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Projet_Voiture_Télécommandée/Client Socket/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.XInput;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static Controller controller;
+        private State State;
+
+        public Form1()
+        {
+            InitializeComponent();
+            controller = new Controller(UserIndex.One);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress ipAddress = IPAddress.Parse(txtHost.Text);
+            int Port = int.Parse(txtPort.Text);
+            Connect1(Port, ipAddress, s);
+        }
+
+        private static void Connect1(int port, IPAddress ip, Socket sock)
+        {
+            
+            Console.WriteLine("Establishing Connection to {0}", ip);
+            sock.Connect(ip, port);
+            Console.WriteLine("Connection established");
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoadController()
+        {
+            int x, y;
+            int LeftTrigger, RightTrigger;
+            int LeftThumbX, RightThumbX;
+            int LeftThumbY, RightThumbY;
+            String Btn, Pad;
+
+            if (!controller.IsConnected)
+            {
+                textBox1.BackColor = Color.Red;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Green;
+                State = controller.GetState();
+                Btn = State.Gamepad.Buttons.ToString();
+                label4.Text = State.Gamepad.ToString() + "\n";
+                label4.Text += "Btn : " + Btn + "\n";
+                LeftTrigger = State.Gamepad.LeftTrigger;
+                RightTrigger = State.Gamepad.RightTrigger;
+                label4.Text += " " + LeftTrigger + " " + RightTrigger + "\n";
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string msg = txtData.Text ;
+            byte[] bytemsg = Encoding.ASCII.GetBytes(msg);
+            s.Send(bytemsg);
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            LoadController();
+            int x, y;
+            int LeftTrigger, RightTrigger;
+            int LeftThumbX, RightThumbX;
+            int LeftThumbY, RightThumbY;
+            String Btn, Pad;
+
+            LeftTrigger = State.Gamepad.LeftTrigger;
+            RightTrigger = State.Gamepad.RightTrigger;
+            Btn = State.Gamepad.Buttons.ToString();
+
+            if (RightTrigger > 0)
+            {
+                string msg = txtData.Text;
+                byte[] bytemsg = Encoding.ASCII.GetBytes("a");
+                s.Send(bytemsg);
+            }
+
+            if (LeftTrigger > 0)
+            {
+                string msg = txtData.Text;
+                byte[] bytemsg = Encoding.ASCII.GetBytes("b");
+                s.Send(bytemsg);
+            }
+
+            if (Btn == "DPadLeft")
+            {
+                string msg = txtData.Text;
+                byte[] bytemsg = Encoding.ASCII.GetBytes("c");
+                s.Send(bytemsg);
+            }
+
+            if (Btn == "DPadRight")
+            {
+                string msg = txtData.Text;
+                byte[] bytemsg = Encoding.ASCII.GetBytes("d");
+                s.Send(bytemsg);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
